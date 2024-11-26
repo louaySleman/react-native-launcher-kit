5f741eef63292c0f7b69da811e1ddcbca3f09a0e
louay <louay.canguru>
2024-11-26 19:30:19 +0300

    [change] Bump version to v2.1.0

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@
  * @author Louay Sleman
  * @contact louayakram12@hotmail.com
  * @linkedin https://www.linkedin.com/in/louay-sleman
- * @version 2.0.0
+ * @version 2.1.0
  * @website https://louaysleman.com
  * @copyright Copyright (c) 2024 Louay Sleman. All rights reserved.
  */
